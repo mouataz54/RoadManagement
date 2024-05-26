9089d4b1cf28419421d2e5d4fb351286eee97ece
mouataz54 <109474490+mouataz54@users.noreply.github.com>
2024-05-26 17:59:50 +0200

    first commit

---
+++ pages/imagedetection.py
@@ -0,0 +1,142 @@
+import os
+import logging
+from pathlib import Path
+from typing import NamedTuple
+
+import cv2
+import numpy as np
+from PIL import Image, ExifTags
+from io import BytesIO
+
+import streamlit as st
+
+# Deep learning framework
+from ultralytics import YOLO
+
+from download import download_file
+
+st.set_page_config(
+    page_title="Image Detection",
+    page_icon="📷",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
+
+HERE = Path(__file__).parent
+ROOT = HERE.parent
+
+logger = logging.getLogger(__name__)
+
+MODEL_URL = "https://github.com/mouataz54/RoadManagement/raw/main/models/ROADDMAGE_YOLOv8s.pt"  # noqa: E501
+MODEL_LOCAL_PATH = ROOT / "./models/YOLOv8_Small_RDD.pt"
+download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=89569358)
+
+# Session-specific caching
+# Load the model
+cache_key = "yolov8smallrdd"
+if cache_key in st.session_state:
+    net = st.session_state[cache_key]
+else:
+    net = YOLO(MODEL_LOCAL_PATH)
+    st.session_state[cache_key] = net
+
+CLASSES = [
+    "Longitudinal Crack",
+    "Transverse Crack",
+    "Alligator Crack",
+    "Potholes"
+]
+
+class Detection(NamedTuple):
+    class_id: int
+    label: str
+    score: float
+    box: np.ndarray
+
+st.title("Road Damage Detection - Image")
+st.write("Detect the road damage in using an Image input. Upload the image and start detecting. This section can be useful for examining baseline data.")
+
+image_file = st.file_uploader("Upload Image", type=['png', 'jpg'])
+
+score_threshold = st.slider("Confidence Threshold", min_value=0.0, max_value=1.0, value=0.5, step=0.05)
+st.write("Lower the threshold if there is no damage detected, and increase the threshold if there is false prediction.")
+
+if image_file is not None:
+
+    # Load the image
+    image = Image.open(image_file)
+    
+    col1, col2 = st.columns(2)
+
+    # Check and correct image orientation
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+        exif = dict(image._getexif().items())
+
+        if exif[orientation] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            image = image.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        # Cases where there is no EXIF data or no orientation tag
+        pass
+    
+    # Perform inference
+    _image = np.array(image)
+    h_ori = _image.shape[0]
+    w_ori = _image.shape[1]
+
+    # Define the cropping margins (in pixels)
+    crop_margin = 80
+
+    # Crop the image to remove the margins
+    cropped_image = _image[crop_margin:h_ori-crop_margin, crop_margin:w_ori-crop_margin]
+
+    # Resize the cropped image to the required input size of the model
+    resized_image = cv2.resize(cropped_image, (640, 640), interpolation=cv2.INTER_AREA)
+
+    # Perform inference on the resized image
+    results = net.predict(resized_image, conf=score_threshold)
+    
+    # Save the results
+    for result in results:
+        boxes = result.boxes.cpu().numpy()
+        detections = [
+           Detection(
+               class_id=int(_box.cls),
+               label=CLASSES[int(_box.cls)],
+               score=float(_box.conf),
+               box=_box.xyxy[0].astype(int),
+            )
+            for _box in boxes
+        ]
+
+    annotated_frame = results[0].plot()
+    _image_pred = cv2.resize(annotated_frame, (w_ori, h_ori), interpolation=cv2.INTER_AREA)
+
+    # Original Image
+    with col1:
+        st.write("#### Image")
+        st.image(_image)
+    
+    # Predicted Image
+    with col2:
+        st.write("#### Predictions")
+        st.image(_image_pred)
+
+        # Download predicted image
+        buffer = BytesIO()
+        _downloadImages = Image.fromarray(_image_pred)
+        _downloadImages.save(buffer, format="PNG")
+        _downloadImagesByte = buffer.getvalue()
+
+        downloadButton = st.download_button(
+            label="Download Prediction Image",
+            data=_downloadImagesByte,
+            file_name="RDD_Prediction.png",
+            mime="image/png"
+        )
